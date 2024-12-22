e2bd04bcd5a45a6e1f1583f08083196325c89612
Malte Franke <malte.franke1@gmail.com>
2024-12-22 15:39:56 -0500

    implemented distributed dataset and corresponding yaml

--- src/rfm_docking/datamodule.py
+++ src/rfm_docking/datamodule.py
@@ -15,7 +15,7 @@ from torch.utils.data import Dataset, DataLoader
 # from torch_geometric.loader import DataLoader
 
 from diffcsp.common.utils import PROJECT_ROOT
-from diffcsp.common.data_utils import get_scaler_from_data_list
+from diffcsp.common.data_utils import get_scaler_from_data_list, StandardScalerTorch
 from rfm_docking.manifold_getter import DockingManifoldGetter
 
 
@@ -192,6 +192,71 @@ class CrystDataModule(pl.LightningDataModule):
             f"{self.num_workers=}, "
             f"{self.batch_size=})"
         )
+
+
+class DistributedDataModule(CrystDataModule):
+    def get_scaler(self, scaler_path):
+        """Have to overwrite this method because we need to train a scaler for each of
+        the distributed files, then join the scalers."""
+        self.scaler = dict()
+
+        # Load once to compute property scaler
+        if scaler_path is None:
+            lattice_scaler_list = []
+            prop_scaler_list = {prop: [] for prop in self.datasets.train.prop}
+            train_dataset = hydra.utils.instantiate(self.datasets.train)
+            for processed_path in train_dataset.processed_files:
+                data_list = torch.load(processed_path)
+
+                lattice_scaler = get_scaler_from_data_list(
+                    data_list, key="scaled_lattice"
+                )
+                lattice_scaler_list.append(lattice_scaler)
+
+                for prop in train_dataset.prop:
+                    prop_scaler = get_scaler_from_data_list(data_list, key=prop)
+                    prop_scaler_list[prop].append(prop_scaler)
+
+            # combine scalers
+            lattice_scaler_mean = torch.stack(
+                [scaler.means for scaler in lattice_scaler_list]
+            ).mean(dim=0)
+            lattice_scaler_std = torch.stack(
+                [scaler.stds for scaler in lattice_scaler_list]
+            ).mean(dim=0)
+            self.lattice_scaler = StandardScalerTorch(
+                lattice_scaler_mean, lattice_scaler_std
+            )
+
+            for prop in train_dataset.prop:
+                prop_scaler_mean = torch.stack(
+                    [scaler.means for scaler in prop_scaler_list[prop]]
+                ).mean(dim=0)
+                prop_scaler_std = torch.stack(
+                    [scaler.stds for scaler in prop_scaler_list[prop]]
+                ).mean(dim=0)
+                self.scaler[prop] = StandardScalerTorch(
+                    prop_scaler_mean, prop_scaler_std
+                )
+
+        else:
+            try:
+                self.lattice_scaler = torch.load(
+                    Path(scaler_path) / "lattice_scaler.pt"
+                )
+                for prop in train_dataset.prop:
+                    self.scaler[prop] = torch.load(
+                        Path(scaler_path) / f"prop_{prop}_scaler.pt"
+                    )
+            except:
+                train_dataset = hydra.utils.instantiate(self.datasets.train)
+                self.lattice_scaler = get_scaler_from_data_list(
+                    train_dataset.cached_data, key="scaled_lattice"
+                )
+                for prop in train_dataset.prop:
+                    self.scaler[prop] = get_scaler_from_data_list(
+                        train_dataset.cached_data, key=prop
+                    )
 
 
 @hydra.main(
