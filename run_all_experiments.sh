212ff849b6d9a9d1933efe28d4724c96bb018862
Malte Franke <malte.franke1@gmx.de>
2025-01-15 16:47:17 +0100

    update run_all_experiments.sh

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export data="dual_docking_data_distributed"
+export model="docking_only_coords"
 
 ### docking
-export model="dock_cspnet"
 export vectorfield="dock_cspnet"
 
 samplings=("uniform" "uniform_then_gaussian" "uniform_then_conformer" "voronoi_then_gaussian" "voronoi_then_conformer")
@@ -12,21 +12,22 @@ doOT=("true" "false")
 
 for sampling in "${samplings[@]}"; do
     for ot in "${doOT[@]}"; do
+        output_file="__Docking_${sampling}_ot_${ot}_%j.txt"
         export sampling=$sampling
         export ot=$ot
-        sbatch run_docking.sh 
+        sbatch --output="$output_file" run_docking.sh 
     done
 done
 
 ### dual docking
-export model="dual_docking_cspnet"
 export vectorfield="dual_docking_cspnet"
 
 samplings=("uniform" "voronoi")
 for sampling in "${samplings[@]}"; do
     for ot in "${doOT[@]}"; do
+        output_file="__DualDocking_${sampling}_ot_${ot}_%j.txt"
         export sampling=$sampling
         export ot=$ot
-        sbatch run_docking.sh
+        sbatch --output="$output_file" run_docking.sh
     done
 done
